e7d3f33477ffbc5f10b13f81db2424776c2da1a7
Chirag Tripathi <87745910+chxrro@users.noreply.github.com>
2025-01-21 12:29:42 -0600

    add command file for coral intake

---
+++ src/main/java/frc/robot/commands/CoralIntakeCommand.java
@@ -0,0 +1,37 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralIntake;
+
+public class CoralIntakeCommand extends Command {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final CoralIntake m_CoralIntake;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public CoralIntakeCommand(CoralIntake subsystem) {
+    m_CoralIntake = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
