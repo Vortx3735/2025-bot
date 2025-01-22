7cbb8d2e64161dbb3c6607f9274ee15cf68d3b08
Andrew <sandrew0208@gmail.com>
2025-01-22 11:36:02 -0600

    Debugging ClimbSubsystem and ClimbCommands

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,14 +3,14 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-//import frc.robot.subsystems.ClimbSubsystem;
+
 import edu.wpi.first.net.WebServer;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+// import frc.robot.subsystems.ClimbSubsystem;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
--- src/main/java/frc/robot/commands/ClimbCommand.java
+++ src/main/java/frc/robot/commands/ClimbCommand.java
@@ -1,52 +1,50 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.ClimbSubsystem;
 
 public class ClimbCommand extends CommandBase {
-    private final ClimbSubsystem climbSubsystem;
-    private final Operation operation;
+  private final ClimbSubsystem climbSubsystem;
+  private final Operation operation;
 
-    public enum Operation {
-        GRAB,
-        LIFT,
-        RELEASE,
-        HOLD
-    }
+  public enum Operation {
+    GRAB,
+    LIFT,
+    RELEASE,
+    HOLD
+  }
 
-    public ClimbCommand(ClimbSubsystem subsystem, Operation operation) {
-        this.climbSubsystem = subsystem;
-        this.operation = operation;
-        addRequirements(climbSubsystem);
-    }
+  public ClimbCommand(ClimbSubsystem subsystem, Operation operation) {
+    this.climbSubsystem = subsystem;
+    this.operation = operation;
+    addRequirements(climbSubsystem);
+  }
 
-    @Override
-    public void initialize() {
-        switch (operation) {
-            case GRAB:
-                climbSubsystem.grab();
-                break;
-            case LIFT:
-                climbSubsystem.lift();
-                break;
-            case RELEASE:
-                climbSubsystem.release();
-                break;
-            case HOLD:
-                climbSubsystem.hold();
-                break;
-        }
+  @Override
+  public void initialize() {
+    switch (operation) {
+      case GRAB:
+        climbSubsystem.grab();
+        break;
+      case LIFT:
+        climbSubsystem.lift();
+        break;
+      case RELEASE:
+        climbSubsystem.release();
+        break;
+      case HOLD:
+        climbSubsystem.hold();
+        break;
     }
+  }
 
-    @Override
-    public void end(boolean interrupted) {
-        climbSubsystem.stop();
-    }
+  @Override
+  public void end(boolean interrupted) {
+    climbSubsystem.stop();
+  }
 
-    @Override
-    public boolean isFinished() {
-        return false; // Adjust logic if needed for ending
-    }
+  @Override
+  public boolean isFinished() {
+    return false; // Adjust logic if needed for ending
+  }
 }
