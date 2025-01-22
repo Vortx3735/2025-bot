32a328c96d1d62cb2390fe1ed9e35c4d34be0ebe
rkoripalli <rkoripalli@hotmail.com>
2025-01-21 21:26:18 -0600

    Added simple unit test for intake that can be used as an example.

---
+++ src/main/java/frc/robot/util/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot.util;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2025-Bot";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "2ef28e564fb7878d9f491388335e4d0a93414450";
+  public static final String GIT_DATE = "2025-01-17 23:40:08 CST";
+  public static final String GIT_BRANCH = "12-implement-choreo-autonomous-routine";
+  public static final String BUILD_DATE = "2025-01-18 09:59:46 CST";
+  public static final long BUILD_UNIX_TIME = 1737215986867L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
