342114a0b97b7c0e234a0f7507b58aaaecb84001
Chirag Tripathi <87745910+chxrro@users.noreply.github.com>
2025-02-08 11:08:50 -0600

    Dev (#37)
    * update some stuff + cleanup
    
    * fix elevator + add button bindings + cleanup
    
    * Fix unit test erros (comment it out)

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -11,15 +11,15 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
-  public static TalonFX elevatorMotor1;
-  public static TalonFX elevatorMotor2;
+  public static TalonFX leftElevatorMotor;
+  public static TalonFX rightElevatorMotor;
   private static CANcoder elevatorEncoder;
   private static PIDController elevatorPIDController = new PIDController(0.1, 0.1, 0.1);
 
-  public Elevator(CANcoder encoder, TalonFX motor1, TalonFX motor2) {
-    elevatorEncoder = encoder;
-    elevatorMotor1 = motor1;
-    elevatorMotor2 = motor2;
+  public Elevator(int encoderID, int leftMotorID, int rightMotorID) {
+    elevatorEncoder = new CANcoder(encoderID);
+    leftElevatorMotor = new TalonFX(leftMotorID);
+    rightElevatorMotor = new TalonFX(rightMotorID);
 
     configureCANcoder();
     configureTalonFX();
@@ -44,18 +44,18 @@ public class Elevator extends SubsystemBase {
         160; // Target acceleration of 160 rps/s (0.5 seconds)
     motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
 
-    elevatorMotor1.getConfigurator().apply(talonFXConfigs);
+    leftElevatorMotor.getConfigurator().apply(talonFXConfigs);
     // create a Motion Magic request, voltage output
     final MotionMagicVoltage m_request = new MotionMagicVoltage(0);
 
     // set target position to 100 rotations
-    elevatorMotor1.setControl(m_request.withPosition(100));
-    elevatorMotor1.getConfigurator().apply(talonFXConfigs);
+    leftElevatorMotor.setControl(m_request.withPosition(100));
+    leftElevatorMotor.getConfigurator().apply(talonFXConfigs);
   }
 
   public double getSelectedSensorPosition() {
-    double position1 = elevatorMotor1.getPosition().getValueAsDouble();
-    double position2 = elevatorMotor2.getPosition().getValueAsDouble();
+    double position1 = leftElevatorMotor.getPosition().getValueAsDouble();
+    double position2 = rightElevatorMotor.getPosition().getValueAsDouble();
     // Assuming you want to return the average position of both motors
     return (position1 + position2) / 2.0;
   }
@@ -81,8 +81,8 @@ public class Elevator extends SubsystemBase {
       fx_cfg.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
       fx_cfg.Feedback.SensorToMechanismRatio = 1.0;
       fx_cfg.Feedback.RotorToSensorRatio = 12.8;
-      elevatorMotor1.getConfigurator().apply(fx_cfg);
-      elevatorMotor2.getConfigurator().apply(fx_cfg);
+      leftElevatorMotor.getConfigurator().apply(fx_cfg);
+      rightElevatorMotor.getConfigurator().apply(fx_cfg);
     } catch (Exception e) {
       System.err.println("Failed to configure TalonFX: " + e.getMessage());
     }
@@ -90,10 +90,10 @@ public class Elevator extends SubsystemBase {
 
   private void logPositions() {
     try {
-      elevatorMotor1.getPosition().getValueAsDouble();
-      elevatorMotor2.getPosition().getValueAsDouble();
+      leftElevatorMotor.getPosition().getValueAsDouble();
+      rightElevatorMotor.getPosition().getValueAsDouble();
       elevatorEncoder.getAbsolutePosition();
-      System.out.println("FX Position: " + elevatorMotor1.getPosition().getValueAsDouble());
+      System.out.println("FX Position: " + leftElevatorMotor.getPosition().getValueAsDouble());
       System.out.println("CANcoder Position: " + elevatorEncoder.getPosition());
     } catch (Exception e) {
       System.err.println("Failed to log positions: " + e.getMessage());
@@ -104,76 +104,72 @@ public class Elevator extends SubsystemBase {
   private static final double LOWERLIMIT = 0.0;
   private static final double UPPERLIMIT = 5.0;
 
-  public class ElevatorControl {
+  public enum ElevatorLevel {
+    LEVEL1(1.0),
+    LEVEL2(2.0),
+    LEVEL3(3.0),
+    LEVEL4(4.0);
 
-    public enum ElevatorLevel {
-      LEVEL1(1.0),
-      LEVEL2(2.0),
-      LEVEL3(3.0),
-      LEVEL4(4.0);
+    private final double position;
 
-      private final double position;
-
-      ElevatorLevel(double position) {
-        this.position = position;
-      }
-
-      public double getPosition() {
-        return position;
-      }
+    ElevatorLevel(double position) {
+      this.position = position;
     }
 
-    public void moveToElevatorLevel(ElevatorControl.ElevatorLevel level) {
-      double position = level.getPosition();
-      moveElevatorToPosition(position);
-    }
-
-    // This method sets both elevatorMotor1 and elevatorMotor2 to move to the specified position
-    // using a motion control algorithm (MotionMagicVoltage). This ensures that both motors move
-    // in a coordinated manner to reach the desired position
-    public void moveElevatorToPosition(double position) {
-      elevatorMotor1.setControl(new MotionMagicVoltage(0).withPosition(position));
-      elevatorMotor2.setControl(new MotionMagicVoltage(0).withPosition(position));
-    }
-
-    public void setElevatorSpeed(double speed) {
-      elevatorMotor1.set(speed);
-      elevatorMotor2.set(speed);
-    }
-
-    public void moveElevatorUp() {
-      elevatorMotor1.set(1);
-      elevatorMotor2.set(1);
-    }
-
-    public void moveElevatorDown() {
-      elevatorMotor1.set(-1);
-      elevatorMotor2.set(-1);
-    }
-
-    public void moveToPostitionLEVEL1() {
-      moveElevatorToPosition(ElevatorControl.ElevatorLevel.LEVEL1.getPosition());
-    }
-
-    public void moveToPostitionLEVEL2() {
-      moveElevatorToPosition(ElevatorControl.ElevatorLevel.LEVEL2.getPosition());
-    }
-
-    public void moveToPostitionLEVEL3() {
-      moveElevatorToPosition(ElevatorControl.ElevatorLevel.LEVEL3.getPosition());
-    }
-
-    public void moveToPostitionLEVEL4() {
-      moveElevatorToPosition(ElevatorLevel.LEVEL4.getPosition());
-    }
-
-    public void stopElevator() {
-      elevatorMotor1.set(0);
-      elevatorMotor2.set(0);
+    public double getPosition() {
+      return position;
     }
   }
 
-  private ElevatorControl elevatorControl = new ElevatorControl();
+  public void moveToElevatorLevel(ElevatorLevel level) {
+    double position = level.getPosition();
+    moveElevatorToPosition(position);
+  }
+
+  // This method sets both leftElevatorMotor and rightElevatorMotor to move to the specified
+  // position
+  // using a motion control algorithm (MotionMagicVoltage). This ensures that both motors move
+  // in a coordinated manner to reach the desired position
+  public void moveElevatorToPosition(double position) {
+    leftElevatorMotor.setControl(new MotionMagicVoltage(0).withPosition(position));
+    rightElevatorMotor.setControl(new MotionMagicVoltage(0).withPosition(position));
+  }
+
+  public void setElevatorSpeed(double speed) {
+    leftElevatorMotor.set(speed);
+    rightElevatorMotor.set(speed);
+  }
+
+  public void moveElevatorUp() {
+    leftElevatorMotor.set(1);
+    rightElevatorMotor.set(1);
+  }
+
+  public void moveElevatorDown() {
+    leftElevatorMotor.set(-1);
+    rightElevatorMotor.set(-1);
+  }
+
+  public void moveToPostitionLEVEL1() {
+    moveElevatorToPosition(ElevatorLevel.LEVEL1.getPosition());
+  }
+
+  public void moveToPostitionLEVEL2() {
+    moveElevatorToPosition(ElevatorLevel.LEVEL2.getPosition());
+  }
+
+  public void moveToPostitionLEVEL3() {
+    moveElevatorToPosition(ElevatorLevel.LEVEL3.getPosition());
+  }
+
+  public void moveToPostitionLEVEL4() {
+    moveElevatorToPosition(ElevatorLevel.LEVEL4.getPosition());
+  }
+
+  public void stopElevator() {
+    leftElevatorMotor.set(0);
+    rightElevatorMotor.set(0);
+  }
 
   @Override
   public void periodic() {
@@ -181,7 +177,7 @@ public class Elevator extends SubsystemBase {
     double position = elevatorEncoder.getAbsolutePosition().getValueAsDouble();
     // Checks if the elevator is is pass limits
     if (position <= LOWERLIMIT || position >= UPPERLIMIT) {
-      elevatorControl.stopElevator();
+      stopElevator();
       System.out.println("Elevator position out of bounds: " + position);
     } else {
       System.out.println("Elevator position: " + position);
